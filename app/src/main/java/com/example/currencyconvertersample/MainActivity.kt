68983a5ddef8e231c99d33d1ffa03a1adce1e492
alshimaarefaat <alshimaa.refaat@ibm.com>
2024-02-15 12:19:04 +0200

    Handling logic of converting currency

--- app/src/main/java/com/example/currencyconvertersample/MainActivity.kt
+++ app/src/main/java/com/example/currencyconvertersample/MainActivity.kt
@@ -3,31 +3,15 @@ package com.example.currencyconvertersample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-
 import com.example.currencyconvertersample.view_model.CurrencyViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -36,110 +20,138 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CurrencyConverterApp()
+            CurrencyConverterScreen()
         }
     }
+}
 
-    @Composable
-    fun CurrencyConverterApp(viewModel: CurrencyViewModel = hiltViewModel()) {
-        // Directly observe MutableState values
-        val currencySymbols by viewModel.currencySymbols
-        val errorMessage by viewModel.errorMessage
+@Composable
+fun CurrencyConverterScreen(viewModel: CurrencyViewModel = hiltViewModel()) {
+    val currencySymbols by viewModel.currencySymbols
+    val errorMessage by viewModel.errorMessage
 
-        Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            errorMessage?.let { message ->
-                Text(
-                    text = message,
-                    style = MaterialTheme.typography.body1,
-                    color = MaterialTheme.colors.error
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-            }
-
-            if (currencySymbols.isNotEmpty()) {
-                CurrencySelectionDropdowns(currencySymbols = currencySymbols, viewModel = viewModel)
-            } else {
-                CircularProgressIndicator()
-            }
-        }
-    }
-
-    @Composable
-    fun CurrencySelectionDropdowns(
-        currencySymbols: Map<String, String>,
-        viewModel: CurrencyViewModel
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Remember expanded state for each dropdown
-        var expandedFrom by remember { mutableStateOf(false) }
-        var expandedTo by remember { mutableStateOf(false) }
+        ErrorMessage(errorMessage)
+        if (currencySymbols.isNotEmpty()) {
+            CurrencySelectionDropdowns(currencySymbols, viewModel)
+        } else {
+            CircularProgressIndicator()
+        }
+    }
+}
 
-        var fromCurrency by remember { mutableStateOf("") }
-        var toCurrency by remember { mutableStateOf("") }
+@Composable
+fun ErrorMessage(errorMessage: String?) {
+    errorMessage?.let {
+        Text(
+            text = it,
+            style = MaterialTheme.typography.body1,
+            color = MaterialTheme.colors.error
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
 
-        // Adjusted DropdownMenu to use the expanded state
+@Composable
+fun CurrencySelectionDropdowns(
+    currencySymbols: Map<String, String>,
+    viewModel: CurrencyViewModel
+) {
+    var expandedFrom by remember { mutableStateOf(false) }
+    var expandedTo by remember { mutableStateOf(false) }
+    var fromCurrency by remember { mutableStateOf("") }
+    var toCurrency by remember { mutableStateOf("") }
+    val amount by viewModel.amount
+    val convertedAmount by viewModel.convertedAmount
+
+    Column {
         Row {
-
-
-                Button(onClick = { expandedFrom = true }) {
-                    Text(if (fromCurrency.isEmpty()) "From" else fromCurrency)
-                }
-
-                DropdownMenu(
-                    expanded = expandedFrom,
-                    onDismissRequest = { expandedFrom = false },
-                    modifier = Modifier.padding(8.dp)
-                ) {
-                    currencySymbols.keys.forEach { currencyCode ->
-                        DropdownMenuItem(onClick = {
-                            fromCurrency = currencyCode
-                            expandedFrom = false
-                        }) {
-                            Text(text = "$currencyCode - ${currencySymbols[currencyCode]}")
-                        }
-                    }
-                }
-
-
-            Spacer(modifier = Modifier.width(10.dp))
-            // Convert button with the action logic
-            Button(
-                onClick = {
-                    // Placeholder for conversion logic
-                    // Ensure you implement the conversion logic in the ViewModel
-                },
-            ) {
-                Text("Convert")
-            }
-            Spacer(modifier = Modifier.width(10.dp))
-
-            Button(onClick = { expandedTo = true }) {
-                Text(if (toCurrency.isEmpty()) "To" else toCurrency)
-            }
-
-            DropdownMenu(
-                expanded = expandedTo,
-                onDismissRequest = { expandedTo = false },
-                modifier = Modifier.padding(8.dp)
-            ) {
-                currencySymbols.keys.forEach { currencyCode ->
-                    DropdownMenuItem(onClick = {
-                        toCurrency = currencyCode
-                        expandedTo = false
-                    }) {
-                        Text(text = "$currencyCode - ${currencySymbols[currencyCode]}")
-                    }
-                }
+            CurrencyDropdown(
+                expanded = expandedFrom,
+                onExpandChange = { expandedFrom = it },
+                selectedCurrency = fromCurrency,
+                onCurrencySelected = { fromCurrency = it },
+                currencySymbols = currencySymbols,
+                label = "From"
+            )
+            AmountInputField(amount, "Amount") {newAmount ->
+               viewModel.onAmountChanged(newAmount,fromCurrency,toCurrency)
             }
         }
 
+        Spacer(modifier = Modifier.width(10.dp))
+        SwapButton()
+        Spacer(modifier = Modifier.width(10.dp))
+        Row {
+            CurrencyDropdown(
+                expanded = expandedTo,
+                onExpandChange = { expandedTo = it },
+                selectedCurrency = toCurrency,
+                onCurrencySelected = { toCurrency = it },
+                currencySymbols = currencySymbols,
+                label = "To"
+            )
+            AmountInputField(convertedAmount, "Converted Amount"){newConvertedAmount->
+               viewModel.onConvertedAmountChanged(newConvertedAmount,fromCurrency,toCurrency)
+            }
+        }
 
     }
+}
 
+@Composable
+fun CurrencyDropdown(
+    expanded: Boolean,
+    onExpandChange: (Boolean) -> Unit,
+    selectedCurrency: String,
+    onCurrencySelected: (String) -> Unit,
+    currencySymbols: Map<String, String>,
+    label: String
+) {
+    Column {
+        Button(onClick = { onExpandChange(true) }) {
+            Text(if (selectedCurrency.isEmpty()) label else selectedCurrency)
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { onExpandChange(false) }
+        ) {
+            currencySymbols.keys.forEach { currencyCode ->
+                DropdownMenuItem(onClick = {
+                    onCurrencySelected(currencyCode)
+                    onExpandChange(false)
+                }) {
+                    Text(text = "$currencyCode - ${currencySymbols[currencyCode]}")
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun SwapButton() {
+    Button(onClick = { /* Implement Swap Logic */ }) {
+        Text("Swap")
+    }
+}
+
+@Composable
+fun AmountInputField(value: String, label: String, onValueChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = { newValue ->
+            if (newValue.all { it.isDigit() || it == '.' }) {
+                onValueChange(newValue)
+            }
+        },
+        label = { Text(label) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier.width(200.dp)
+    )
 }
--- app/src/main/java/com/example/currencyconvertersample/view_model/CurrencyViewModel.kt
+++ app/src/main/java/com/example/currencyconvertersample/view_model/CurrencyViewModel.kt
@@ -1,10 +1,6 @@
 package com.example.currencyconvertersample.view_model
 
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.viewModelScope
@@ -25,6 +21,12 @@ class CurrencyViewModel @Inject constructor(
     var errorMessage = mutableStateOf<String?>(null)
         private set
 
+    var convertedAmount = mutableStateOf<String>("")
+        private set
+
+    var amount = mutableStateOf<String>("1")
+        private set
+
     init {
         loadCurrencySymbols()
     }
@@ -33,8 +35,7 @@ class CurrencyViewModel @Inject constructor(
         viewModelScope.launch {
             try {
                 if (isNetworkAvailable()) {
-                    // Assume you have an API key
-                    val apiKey = "5a8dfda0e2efa71172799eace09bfd52"
+                    val apiKey = "ddcc54d739557a79b5b81c5e75a9b13a"
                     val result = repository.getCurrencySymbols(apiKey)
                     currencySymbols.value = result ?: mapOf()
                     errorMessage.value = null // Reset error message on successful load
@@ -42,9 +43,8 @@ class CurrencyViewModel @Inject constructor(
                     errorMessage.value = "No internet connection"
                 }
             } catch (e: Exception) {
-                // Handle the error, e.g., by setting an error message
                 errorMessage.value = "Failed to load currency symbols: ${e.message}"
-                // Optionally, log the error or handle it as needed for your use case
+
             }
         }
     }
@@ -54,8 +54,51 @@ class CurrencyViewModel @Inject constructor(
         }
 
 
-    // You might want to expose a method to reload data in case of an error
-    fun reloadData() {
+    /*fun reloadData() {
         loadCurrencySymbols()
     }
+*/
+
+
+
+    fun onAmountChanged(newAmount: String, fromCurrency: String, toCurrency: String) {
+        amount.value = newAmount
+        val rate = getConversionRate(fromCurrency, toCurrency)
+        val result = newAmount.toDoubleOrNull()?.times(rate) ?: 0.0
+        convertedAmount.value = result.toString()
+    }
+
+    fun onConvertedAmountChanged(newConvertedAmount: String, fromCurrency: String, toCurrency: String) {
+        convertedAmount.value = newConvertedAmount
+        val rate = getConversionRate(fromCurrency,toCurrency)
+        val result=  newConvertedAmount.toDoubleOrNull()?.div(rate)?: 0.0
+        amount.value = result.toString()
+
+    }
+    private val conversionRates = mapOf(
+        "USD_TO_EUR" to 0.85,
+        "EUR_TO_USD" to 1.18,
+        "USD_TO_JPY" to 110.0,
+        "JPY_TO_USD" to 0.0091,
+        "EUR_TO_GBP" to 0.86,
+        "GBP_TO_EUR" to 1.16,
+        "USD_TO_GBP" to 0.73,
+        "GBP_TO_USD" to 1.37, // Example rate
+        // Add more based on necessity
+    )
+
+    private fun getConversionRate(fromCurrency: String, toCurrency: String): Double {
+        val key = "${fromCurrency}_TO_$toCurrency"
+        return conversionRates[key] ?: 2.0
+    }
+
+    fun swapCurrencies() {
+  /*      val temp = fromCurrency.value
+        fromCurrency.value = toCurrency.value
+        toCurrency.value = temp
+        // Trigger conversion calculation
+        onAmountChanged(amount.value, fromCurrency.value, toCurrency.value)*/
+    }
+
+
 }
